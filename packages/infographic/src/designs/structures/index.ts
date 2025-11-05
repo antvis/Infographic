93c822154aea7f7b7157252abc197c175db4245b
Aaron <antvaaron@gmail.com>
2025-11-05 17:12:45 +0800

    refactor(infograpic): migrate chart column and pyramid to structure

--- packages/infographic/src/designs/structures/index.ts
+++ packages/infographic/src/designs/structures/index.ts
@@ -1,3 +1,4 @@
+export * from './chart-column';
 export * from './compare-hierarchy-left-right';
 export * from './compare-hierarchy-row';
 export * from './compare-left-right';
@@ -19,6 +20,7 @@ export * from './sequence-circle-arrows';
 export * from './sequence-color-snake-steps';
 export * from './sequence-cylinders-3d';
 export * from './sequence-horizontal-zigzag';
+export * from './sequence-pyramid';
 export * from './sequence-roadmap-vertical';
 export * from './sequence-snake-steps';
 export * from './sequence-steps';
