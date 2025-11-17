54f4216cf948717bb6fd9e1d60f340755c46765c
Aaron <antvaaron@gmail.com>
2025-11-17 18:43:28 +0800

    chore: add umd build

---
+++ packages/infographic/vite.config.ts
@@ -0,0 +1,34 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@antv/infographic-jsx': resolve(__dirname, '../jsx/src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['umd'],
+      name: 'AntVInfographic',
+      fileName: () => 'infographic.min.js',
+    },
+    rollupOptions: {
+      output: {
+        exports: 'named',
+      },
+    },
+    minify: 'esbuild',
+    sourcemap: true,
+    target: 'es2015',
+    outDir: 'dist',
+    emptyOutDir: false,
+  },
+  esbuild: {
+    target: 'es2015',
+    minifyIdentifiers: true,
+    minifySyntax: true,
+    minifyWhitespace: true,
+  },
+});
