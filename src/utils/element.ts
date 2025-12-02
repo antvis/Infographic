94f79a3f41e7a05bb99276c06b93f21da05feae0
Aaron <antvaaron@gmail.com>
2025-12-02 16:01:19 +0800

    refactor(utils): upate element utils parameter types

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -1,14 +1,22 @@
 import { ElementTypeEnum } from '../constants';
 
-export function setElementRole(element: SVGElement, role: string) {
+export function setElementRole(
+  element: SVGElement | HTMLElement,
+  role: string,
+) {
   element.setAttribute('data-element-type', role);
 }
 
-export function getElementByRole(element: SVGElement, role: string) {
+export function getElementByRole(
+  element: SVGElement | HTMLElement,
+  role: string,
+) {
   return element.querySelector(`[data-element-type="${role}"]`);
 }
 
-export function getElementRole(element: SVGElement): ElementTypeEnum {
+export function getElementRole(
+  element: SVGElement | HTMLElement,
+): ElementTypeEnum {
   return (
     (element.getAttribute('data-element-type') as ElementTypeEnum) ||
     ElementTypeEnum.Unknown
