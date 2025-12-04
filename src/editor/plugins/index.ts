0a7ca3ea628521e56c4ca9416ada9b73740bc30c
Aaron <antvaaron@gmail.com>
2025-12-04 11:09:18 +0800

    feat(editor): implement resize element plugin

--- src/editor/plugins/index.ts
+++ src/editor/plugins/index.ts
@@ -1,1 +1,2 @@
 export * from './edit-bar';
+export * from './resize-element';
--- src/runtime/options.ts
+++ src/runtime/options.ts
@@ -2,12 +2,13 @@ import {
   ClickSelect,
   DblClickEditText,
   EditBar,
+  ResizeElement,
   SelectHighlight,
 } from '../editor';
 import { InfographicOptions } from '../options';
 
 export const DEFAULT_OPTIONS: Partial<InfographicOptions> = {
-  plugins: [new EditBar()],
+  plugins: [new EditBar(), new ResizeElement()],
   interactions: [
     new DblClickEditText(),
     new ClickSelect(),
