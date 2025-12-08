2c70928ddd8f4b86c78d14a8851cc301a2e16ece
Aaron <antvaaron@gmail.com>
2025-12-08 16:09:49 +0800

    refactor(editor): adjust edit bar icons

--- src/editor/plugins/edit-bar/edit-items/font-size.ts
+++ src/editor/plugins/edit-bar/edit-items/font-size.ts
@@ -1,7 +1,7 @@
 import type { TextAttributes } from '../../../../types';
 import { injectStyleOnce } from '../../../../utils';
 import { UpdateElementCommand } from '../../../commands';
-import { Aa, IconButton, Popover } from '../components';
+import { IconButton, Popover, TEXT_ICONS } from '../components';
 import type { Button } from '../components/button';
 import type { Icon } from '../components/icons';
 import type { EditItem } from './types';
@@ -33,7 +33,7 @@ export const FontSize: EditItem<TextAttributes> = (
 ) => {
   injectStyleOnce(FONT_SIZE_STYLE_ID, FONT_SIZE_STYLES);
 
-  const button = IconButton({ icon: Aa });
+  const button = IconButton({ icon: TEXT_ICONS.fontSize });
   const currentSize = normalizeFontSize(attrs['font-size']);
   const content = createFontSizeContent(currentSize, (size) => {
     commander.executeBatch(
